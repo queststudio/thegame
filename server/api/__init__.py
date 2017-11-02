e12734f15af904413c4a0b7527c179184013953a
dmigo <d.f.goryunov@gmail.com>
2017-11-02 01:05:18 +0100

    Fix servo ranges 2

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -7,12 +7,14 @@ import time
 SERVO_MIN = 150
 SERVO_MAX = 600
 
+
 class Config(object):
     DEBUG = True
     TESTING = False
     PRESERVE_CONTEXT_ON_EXCEPTION = True
     PROPAGATE_EXCEPTIONS = True
     LOGGER_NAME = 'api'
+
 
 app = Flask(__name__)
 CORS(app)
@@ -23,17 +25,20 @@ api = Api(app)
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
 
-def get_manometer_target(target):
-    trimmed = 0 if target<0 else 100 if target>100 else target
-    range = SERVO_MAX - SERVO_MIN
-    real_target = SERVO_MIN + trimmed*range/100
-    return int(real_target)
 
 def get_ventile_target(target):
-    trimmed = 0 if target<0 else 25 if target>25 else target
+    trimmed = 0 if target < 0 else 100 if target > 100 else target
     range = SERVO_MAX - SERVO_MIN
-    real_target = SERVO_MIN + trimmed*range/25
+    real_target = SERVO_MIN + trimmed * range / 100
     return int(real_target)
+
+
+def get_manometer_target(target):
+    trimmed = 0 if target < 0 else 25 if target > 25 else target
+    range = SERVO_MAX - SERVO_MIN
+    real_target = SERVO_MIN + trimmed * range / 25
+    return int(real_target)
+
 
 class Root(Resource):
     def get(self):
@@ -42,11 +47,16 @@ class Root(Resource):
     def post(self):
         data = request.get_json()
         servos = data.get('servos')
-        for i in range(0, len(servos)):
-            target = get_real_target(servos[i])
-            print('moving servo {} to {}({})'.format(i,servos[i],target))
+        for i in range(0, 3):
+            target = get_manometer_target(servos[i])
+            print('moving servo {} to {}({})'.format(i, servos[i], target))
+            pwm.set_pwm(i, 0, target)
+        for i in range(3, len(servos)):
+            target = get_ventile_target(servos[i])
+            print('moving servo {} to {}({})'.format(i, servos[i], target))
             pwm.set_pwm(i, 0, target)
         time.sleep(1)
         return {'status': 'success'}
+
 
 api.add_resource(Root, '/')
