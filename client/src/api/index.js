bf1057c3a8b7e39aab4bde99a109c6d11459b944
dmigo <d.f.goryunov@gmail.com>
2018-06-01 23:14:52 +0200

    Fetch colocated api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,5 @@
 import rest from 'rest'
 import mime from 'rest/interceptor/mime'
-import { getQueryString } from '../utils'
 
 const client = rest.wrap(mime, { mime: 'application/json' })
 const waitASec = () => {
@@ -12,16 +11,8 @@ const waitASec = () => {
 }
 
 export const check = () => {
-  const host = getQueryString('host')
-  const port = getQueryString('port')
-
-  client({ path: `http://${host}:${port}/api` })
+  client({ path: `/api` })
 }
 export const servos = servos => {
-  const host = getQueryString('host')
-  const port = getQueryString('port')
-
-  return host && port
-    ? client({ path: `http://${host}:${port}/api`, entity: { servos } })
-    : waitASec()
+  return client({ path: `/api`, entity: { servos } })
 }
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,17 +1,10 @@
 export const guid = () =>
   'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+      v = c == 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 
 export const random = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-export const getQueryString =  ( field, url ) => {
-  var href = url ? url : window.location.href;
-  var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
-  var string = reg.exec(href);
-  return string ? string[1] : null;
-};
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
